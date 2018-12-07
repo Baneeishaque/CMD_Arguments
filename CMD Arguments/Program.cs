bbf6922a4517289feb37b33247724700667071cd
Banee Ishaque K <Baneeishaque@gmail.com>
2018-12-07 17:44:04 +0530

    Add project files.

---
+++ CMD Arguments/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMD_Arguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+            if (args.Length != 0)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    Console.WriteLine(args[i]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
